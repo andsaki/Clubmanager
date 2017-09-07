9ecf03f0eb3ef15857b3befec3f0970be9faa838
Naruchika Kikuya <n.kiku@y3.dion.ne.jp>
2017-09-07 10:47:35 +0900

    ユーザーページ更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
   get 'events/index'
   get 'events/new'
   post 'events' => 'events#create'
   get 'events/show/:e_id' => 'events#show'
-  
+
+
+
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, :only => [:index, :show]
 
 resources :posts do
   resources :comments, only: [:create, :destroy]
