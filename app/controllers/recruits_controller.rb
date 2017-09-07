ffe901840c5de5cb0142bc9b06e69719e46bc08c
Masataka Murota <balius.1064@gmail.com>
2017-09-07 15:44:00 +0900

    indexの分割

--- app/controllers/recruits_controller.rb
+++ app/controllers/recruits_controller.rb
@@ -1,26 +1,45 @@
 class RecruitsController < ApplicationController
 
   def index
-    @recruits = Post.all.order(created_at: 'desc')
-    @events = Event.all
+    @recruits = Recruit.all.order(created_at: 'desc')
   end
 
   def show
+    @recruits = Recruit.find(params[:id])
   end
 
   def new
+    @recruits = Recruit.new
   end
 
   def edit
+    @recruits = Recruit.find(params[:id])
   end
 
-  def create_table
+  def create
+    @recruits = Recruit.new(post_params)
+    if @recruits.save
+      redirect_to recruits_path
+    else
+      render 'new'
+    end
   end
 
   def update
+    @recruits = Recruit.find(params[:id])
+    if @recruits.update(recruit_params)
+      redirect_to recruits_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    def destroy
+      @recruits = Recruit.find(params[:id])
+      @recruits.destroy
+      redirect_to recruits_path
+    end
   end
 
 end
