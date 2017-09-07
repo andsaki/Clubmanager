356a88a711d90c208ae6185a96b7931c9f5ba9bf
Masataka Murota <balius.1064@gmail.com>
2017-09-07 12:43:37 +0900

    募集の追加

---
+++ app/controllers/recruits_controller.rb
@@ -0,0 +1,24 @@
+class RecruitsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create_table
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+    
+end
