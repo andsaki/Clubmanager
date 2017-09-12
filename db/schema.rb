4e99389ae79e0792e238cfe714adc4429a09755d
Doi Shuhei <doishu624@gmail.com>
2017-09-12 15:55:41 +0900

    state_group_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170911091635) do
+ActiveRecord::Schema.define(version: 20170912063606) do
 
   create_table "attendances", force: :cascade do |t|
     t.string "e_id"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20170911091635) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "group_id"
   end
 
   create_table "groups", force: :cascade do |t|
@@ -110,6 +111,7 @@ ActiveRecord::Schema.define(version: 20170911091635) do
     t.string "image"
     t.integer "month"
     t.integer "day"
+    t.integer "state_group_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
