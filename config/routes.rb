5fca9c7dc49408b00dc30b18d85295c4326f87c2
Naruchika Kikuya <n.kiku@y3.dion.ne.jp>
2017-09-12 16:42:15 +0900

    メール送信

--- config/routes.rb
+++ config/routes.rb
@@ -47,10 +47,10 @@ Rails.application.routes.draw do
   end
 
 
-resources :posts do
-  resources :comments, only: [:create, :destroy]
-end
+  resources :posts do
+    resources :comments, only: [:create, :destroy]
+  end
 
- root 'groups#index'
+  root 'groups#index'
 
 end
