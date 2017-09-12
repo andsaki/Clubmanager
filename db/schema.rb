fb1965f30e846947067ada7969288dc6438e50f6
Naruchika Kikuya <n.kiku@y3.dion.ne.jp>
2017-09-12 19:06:02 +0900

    メール設定

--- db/schema.rb
+++ db/schema.rb
@@ -70,6 +70,15 @@ ActiveRecord::Schema.define(version: 20170912063606) do
     t.integer "group_id"
   end
 
+  create_table "recruits", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.string "name"
+    t.boolean "administrator", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
