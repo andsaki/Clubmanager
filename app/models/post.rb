c581cf06d2821aecf90da48db5047108d76c68c8
Masataka Murota <balius.1064@gmail.com>
2017-09-13 11:53:59 +0900

    修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   # @post.comments とかけるようになる
-  validates :title, presence: true, length: { minimum: 3, message: 'Too short to post!'}
+  validates :title, presence: true, length: { minimum: 3, message: 'タイトルが短すぎます'}
   validates :body, presence: true
 end
