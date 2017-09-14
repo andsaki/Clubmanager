28573c8cee035d28eec2df050c3150482038df3f
Doi Shuhei <doishu624@gmail.com>
2017-09-14 12:01:36 +0900

    カレンダー

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,4 @@
 class Event < ApplicationRecord
-  validates :title, presence: true, length: { minimum: 3, message: 'Too short!'}
-  validates :about, presence: true, length: { minimum: 4, message: 'Toooooo'}
+  validates :title, presence: true
+  validates :about, presence: true
 end
